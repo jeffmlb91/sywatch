8bc5fb3fed58c93acfc5ed758674e1fd0658d1d6
jeffmlb91 <66559059+jeffmlb91@users.noreply.github.com>
2024-12-18 00:06:34 -0500

    initialize repo & first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+   <div>
+    We are ready 
+    <Button variant="destructive">Click</Button>
+   </div>
+  )
+}
+
+export default App
